83f01f95aef33765d7c4886e47c9d2cf10a8983a
dicarve <dicarve@dicarve-laptop.(none)>
2009-09-03 14:45:26 +0700

    Upgrade script for stable11 revision

--- upgrade/upgrade_stable11.sql
+++ upgrade/upgrade_stable11.sql
@@ -1,4 +1,5 @@
 -- SENAYAN 3.0 stable 11
 -- Senayan SQL Database upgrade script
 
+TRUNCATE TABLE `setting`;
 ALTER TABLE `biblio` CHANGE `labels` `labels` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
