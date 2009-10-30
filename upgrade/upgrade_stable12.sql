44489a6cb5f4bdc7f8a03586353662f7996b7a35
Hendro Wicaksono <hendrowicaksono@yahoo.com>
2009-10-30 09:17:20 +0700

    Update SQL statement in upgrade/upgrade_stable12.sql

--- upgrade/upgrade_stable12.sql
+++ upgrade/upgrade_stable12.sql
@@ -3,10 +3,10 @@
 -- NOTE: Changes in table 'content' for stable12 are not reflected in this upgrade script
 
 -- biblio_attachment table change
--- ALTER TABLE `biblio_attachment` ADD `access_limit` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL AFTER `access_type`;
+ALTER TABLE `biblio_attachment` ADD `access_limit` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL AFTER `access_type`;
 
 -- member table change
--- ALTER TABLE `member` ADD `last_login_ip` VARCHAR(20) NULL AFTER `is_pending`;
--- ALTER TABLE `member` ADD `last_login` DATETIME NULL AFTER `is_pending`;
--- ALTER TABLE `member` ADD `mpasswd` CHAR(32) NULL AFTER `is_pending`;
+ALTER TABLE `member` ADD `last_login_ip` VARCHAR(20) NULL AFTER `is_pending`;
+ALTER TABLE `member` ADD `last_login` DATETIME NULL AFTER `is_pending`;
+ALTER TABLE `member` ADD `mpasswd` CHAR(32) NULL AFTER `is_pending`;
 
