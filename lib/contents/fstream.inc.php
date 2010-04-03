2ccbd04b16bb2f0f3465f2604c7b6c89cc6074d6
Arie Nugraha <dicarve@DicarveMacbook.local>
2010-04-03 19:27:02 +0700

    Fixed: error generating SWF when PDF file contain spaces

--- lib/contents/fstream.inc.php
+++ lib/contents/fstream.inc.php
@@ -57,11 +57,11 @@ if ($file_q->num_rows > 0) {
             $swf = $swf.'.swf';
             if (!file_exists('files/swfs/'.$swf.'')) {
                 if (stripos(PHP_OS, 'Darwin') !== false) {
-                    exec('lib/swftools/bin/darwin/pdf2swf -o files/swfs/'.$swf.' '.$file_loc.'');
+                    exec('lib/swftools/bin/darwin/pdf2swf -o files/swfs/'.$swf.' "'.$file_loc.'"');
                 } else if (stripos(PHP_OS, 'Linux') !== false) {
-                    exec('lib/swftools/bin/linux/pdf2swf -o files/swfs/'.$swf.' '.$file_loc.'');
+                    exec('lib/swftools/bin/linux/pdf2swf -o files/swfs/'.$swf.' "'.$file_loc.'"');
                 } else {
-                    exec('lib\swftools\bin\windows\pdf2swf.exe -o files/swfs/'.$swf.' '.$file_loc.'');
+                    exec('lib\swftools\bin\windows\pdf2swf.exe -o files/swfs/'.$swf.' "'.$file_loc.'"');
                 }
             }
 
