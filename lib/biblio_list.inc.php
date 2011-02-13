d95f59847423d1ad70647de8da20e9e300762b13
Arie Nugraha <dicarve@gmail.com>
2011-02-14 03:12:29 +0700

    Fixed: undefined index of is_phrase array index

--- lib/biblio_list.inc.php
+++ lib/biblio_list.inc.php
@@ -205,7 +205,7 @@ class biblio_list extends biblio_list_model
                         } else { $_sql_criteria .= " biblio.gmd_id IN ($_subquery)"; }
                         break;
                     case 'notes' :
-			$_q = $_query['is_phrase']?'"'.$_q.'"':$_q;
+			$_q = isset($_query['is_phrase'])?'"'.$_q.'"':$_q;
                         if ($_b == '-') {
                             $_sql_criteria .= " NOT (MATCH (biblio.notes) AGAINST ('".$_q."' IN BOOLEAN MODE))";
                         } else { $_sql_criteria .= " (MATCH (biblio.notes) AGAINST ('".$_q."' IN BOOLEAN MODE))"; }
