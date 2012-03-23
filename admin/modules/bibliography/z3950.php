834afe3ed2285464d24abcf9f10ce25598c2df1e
Eddy Subratha <eddy.subratha@gmail.com>
2012-03-23 21:59:54 +0700

    Fixed : Modified p2p, z3950 and z3950sru layout

--- admin/modules/bibliography/z3950.php
+++ admin/modules/bibliography/z3950.php
@@ -272,15 +272,18 @@ if (isset($_GET['keywords']) AND $can_read) {
 ?>
 <fieldset class="menuBox">
 <div class="menuBoxInner biblioIcon">
-    Z3950
-    <hr />
+    <div class="per_title">
+    	<h2>Z3950</h2>
+    </div>
+    <div class="sub_section">
     <form name="search" id="search" action="<?php echo MODULES_WEB_ROOT_DIR; ?>bibliography/z3950.php" loadcontainer="searchResult" method="get" style="display: inline;"><?php echo __('Search'); ?> :
     <input type="text" name="keywords" id="keywords" size="30" />
     <select name="field"><option value="isbn"><?php echo __('ISBN/ISSN'); ?></option><option value="ti"><?php echo __('Title/Series Title'); ?></option><option value="au"><?php echo __('Authors'); ?></option></select>
     <?php echo __('Server'); ?>: <select name="z3950_source" style="width: 20%;"><?php foreach ($sysconf['z3950_source'] as $serverid => $z3950_source) { echo '<option value="'.$z3950_source['uri'].'">'.$z3950_source['name'].'</option>';  } ?></select>
     <input type="submit" id="doSearch" value="<?php echo __('Search'); ?>" class="button" />
     </form>
-    <div><?php echo __('* Please make sure you have a working Internet connection.'); ?></div>
+    </div>
+    <div class="infoBox"><?php echo __('* Please make sure you have a working Internet connection.'); ?></div>
 </div>
 </fieldset>
 <div id="searchResult">&nbsp;</div>
