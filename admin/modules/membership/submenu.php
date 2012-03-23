5005d0defe2571e062878a29de207f5cbf465507
Arif Syamsudin <buitenzorg812@gmail.com>
2012-03-23 21:53:02 +0700

    Fixed: Export submenu in Membership

--- admin/modules/membership/submenu.php
+++ admin/modules/membership/submenu.php
@@ -29,5 +29,5 @@ $menu[] = array(__('Add New Member'), MODULES_WEB_ROOT_DIR.'membership/index.php
 $menu[] = array(__('Member Type'), MODULES_WEB_ROOT_DIR.'membership/member_type.php', __('View and modify member type'));
 $menu[] = array('Header', __('Tools'));
 $menu[] = array(__('Member Card Printing'), MODULES_WEB_ROOT_DIR.'membership/member_card_generator.php', __('Print Member Card'));
-$menu[] = array(__('Import Data'), MODULES_WEB_ROOT_DIR.'membership/import.php', __('Import Members Data From CSV File'));
 $menu[] = array(__('Export Data'), MODULES_WEB_ROOT_DIR.'membership/export.php', __('Export Members Data To CSV File'));
+$menu[] = array(__('Import Data'), MODULES_WEB_ROOT_DIR.'membership/import.php', __('Import Members Data From CSV File'));
