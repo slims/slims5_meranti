c9f6952faa5a42336dd50addaef4053b761a2373
Aril Apria Susanto <awriel@gmail.com>
2012-03-28 21:46:11 +0700

    Add more filter report in visitor_report_day.php

--- admin/modules/reporting/customs/visitor_report_day.php
+++ admin/modules/reporting/customs/visitor_report_day.php
@@ -105,6 +105,7 @@ if (!$reportView) {
         </div>
     </div>
     <div style="padding-top: 10px; clear: both;">
+    <input type="button" class="button" name="moreFilter" value="<?php echo __('Show More Filter Options'); ?>" />
     <input type="submit" name="applyFilter" value="<?php echo __('Apply Filter'); ?>" />
     <input type="hidden" name="reportView" value="true" />
     </div>
