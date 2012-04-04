49c431f53cfb622d2bdcb1e1c2f976e7d058ad50
Hendro Wicaksono <hendrowicaksono@yahoo.com>
2012-04-04 15:22:25 +0700

    Update SQL upgrade script

--- upgrade/upgrade_slims5_meranti.sql
+++ upgrade/upgrade_slims5_meranti.sql
@@ -1,3 +1,3 @@
 ALTER TABLE  `mst_topic` ADD  `classification` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT  'Classification Code' AFTER  `auth_list` ;
-ALTER TABLE `biblio` ADD `sor` VAR( 200 ) COLLATE utf8_unicode_ci NULL AFTER `title` ;
+ALTER TABLE `biblio` ADD `sor` VARCHAR( 200 ) COLLATE utf8_unicode_ci NULL AFTER `title` ;
 ALTER TABLE `biblio` DROP INDEX `references_idx` ,ADD INDEX `references_idx` ( `gmd_id` , `publisher_id` , `language_id` , `publish_place_id`) ;
