2e56f34685927cc5ac8465501b76a11247a2c1fc
Arie Nugraha <dicarve@gmail.com>
2012-09-22 07:26:43 +0700

    Added: UCS status alert after deleting record(s)

--- ucnode.inc.php
+++ ucnode.inc.php
@@ -27,7 +27,7 @@ if (!defined('INDEX_AUTH')) {
     die("can not access this file directly");
 }
 
-$ucs['serveraddr'] = 'http://127.0.0.1/ucs-2.0'; // UCS server address. NO TRAILING SLASH! for local testing on Windows machine don't use localhost, use 127.0.0.1 instead
+$ucs['serveraddr'] = 'http://127.0.0.1/ucs2'; // UCS server address. NO TRAILING SLASH! for local testing on Windows machine don't use localhost, use 127.0.0.1 instead
 $node['id'] = 'slims-node'; // node ID
 $node['password'] = '2325f677e21c1613909c953eb03c57352259cc5d'; // default is s0beautifulday
 $node['name'] = 'SLiMS Library'; // node name
