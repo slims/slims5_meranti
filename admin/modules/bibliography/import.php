65f8b3aef4e811f83421e2acd2951d15480788b1
Hendro Wicaksono <hendrowicaksono@yahoo.com>
2012-12-24 16:39:55 +0700

    Adding Statement of Responsibility field to be able to be imported in SLiMS biblio csv

--- admin/modules/bibliography/import.php
+++ admin/modules/bibliography/import.php
@@ -137,7 +137,7 @@ if (isset($_POST['doImport'])) {
               $classification = $field[11]?'\''.$field[11].'\'':'NULL';;
               $notes = $field[12]?'\''.$field[12].'\'':'NULL';;
               $image = $field[13]?'\''.$field[13].'\'':'NULL';
-              $file_att = $field[14]?'\''.$field[14].'\'':'NULL';
+              $sor = $field[14]?'\''.$field[14].'\'':'NULL';
               // $authors = preg_replace('@\\\s*'.$field_enc.'$@i', '', $field[15]);
               $authors = trim($field[15]);
               $subjects = trim($field[16]);
@@ -147,12 +147,12 @@ if (isset($_POST['doImport'])) {
                   isbn_issn, publisher_id, publish_year,
                   collation, series_title, call_number,
                   language_id, publish_place_id, classification,
-                  notes, image, file_att, input_date, last_update)
+                  notes, image, sor, input_date, last_update)
                       VALUES ($title, $gmd_id, $edition,
                       $isbn_issn, $publisher_id, $publish_year,
                       $collation, $series_title, $call_number,
                       $language_id, $publish_place_id, $classification,
-                      $notes, $image, $file_att, $curr_datetime, $curr_datetime)";
+                      $notes, $image, $sor, $curr_datetime, $curr_datetime)";
               // send query
               $dbs->query($sql_str);
               $biblio_id = $dbs->insert_id;
